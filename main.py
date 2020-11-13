372d7edcf07e98b19d430cdf5388769c06084544
Grzegorz <grzegorz.libiszewski7@gmail.com>
2020-11-13 11:57:58 +0100

    sleep relatively to modules execution time

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
+from time import time, sleep
 
 from Camera import Camera
 from Commander import Commander
@@ -9,15 +9,17 @@ camera = Camera()
 handler = ModuleHandler(camera)
 commander = Commander(handler)
 while commander.run:
+    start_time = time()
     fps = handler.getMode('f')
-
-    # start_time = time.time()
 
     if not handler.getMode('p'):
         img = camera.readCamera()
         img = handler.tick(img)
         camera.printFrame(img)
 
-    # print("%.3f - %.3f" % ((time.time() - start_time), fps))
-
-    time.sleep(fps)
+    running_time = (time() - start_time)
+    break_time = fps - running_time
+    if break_time < 0:
+        break_time = 0
+    sleep(break_time)
+    # print("%.3f - %.3f" % ((time() - start_time), fps))
